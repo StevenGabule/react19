d04e177ca29fce8b5ce0248531db7a85cc638250
John Paul Gabule <lucas.gabule@gmail.com>
2025-04-12 23:11:57 +0800

    chore: completed the selector instruments

---
+++ src/apps/piano/components/instrument-selector/instrument-selector.tsx
@@ -0,0 +1,17 @@
+import React, { ChangeEvent } from 'react';
+import { useInstrument } from '../../state/instrument';
+import { InstrumentName } from 'soundfont-player';
+import { options } from './options';
+
+export const InstrumentSelector: React.FC = () => {
+	const {instrument, setInstrument} = useInstrument()
+	const updateValue = ({target} : ChangeEvent<HTMLSelectElement>) => setInstrument(target.value as InstrumentName)
+
+	return (
+		<select className="instruments" onChange={updateValue} value={instrument}>
+			{options.map(({label, value}) => (
+				<option value={value} key={value}>{label}</option>
+			))}
+		</select>
+	)
+}
